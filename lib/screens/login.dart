37926f5beb187d4906e161e7a3cdeba15a269605
ConnorBkart <124602638+ConnorBurkart@users.noreply.github.com>
2024-03-25 09:17:25 -0400

    Login Page
    Made more padding and text box size.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,66 +14,73 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void _login() 
-  {
+  void _login() {
     Navigator.pushReplacement(
-      context,
-      CupertinoPageRoute(builder: (context) => const HomeView())
-    );
+        context, CupertinoPageRoute(builder: (context) => const HomeView()));
   }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column( // Using ListView to avoid overflow on smaller devices
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.3, // Adjust the size to 30% of the screen height
-                    child: Image.asset('assets/img/static-paw-003940-c_orange_1.jpg', fit: BoxFit.contain),
-                  ),
-                  CupertinoTextField(
-                    controller: _usernameController,
-                    placeholder: 'Username',
-                    style: const TextStyle(color: Colors.black),
-                  ),
-                  CupertinoTextField(
-                    controller: _passwordController,
-                    placeholder: 'Password',
-                    style: const TextStyle(color: Colors.black),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: _login,
-                    child: const Text('Sign in', style: TextStyle(color: Colors.black)),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+        child: SafeArea(
+      child: Column(
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                  // Using ListView to avoid overflow on smaller devices
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height *
+                          0.3, // Adjust the size to 30% of the screen height
+                      child: Image.asset('assets/img/static-paw-003940-c_orange_1.jpg',
+                          fit: BoxFit.contain),
                     ),
-                  ),
-                  const SizedBox(height: 20), 
-                  Center( // Wrap with Center to align to the middle
-                    child: GestureDetector(
-                      onTap: () {
-                        // Add your logic for "Forget password" here
-                      },
-                      child: const Text(
-                        'Forget password?',
-                        style: TextStyle(color: Colors.black, decoration: TextDecoration.underline),
+                    SizedBox(height: 20),
+                    CupertinoTextField(
+                      controller: _usernameController,
+                      placeholder: 'Username',
+                      style: const TextStyle(color: Colors.black),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 15), // Increased padding
+                    ),
+                    SizedBox(height: 20),
+                    CupertinoTextField(
+                      controller: _passwordController,
+                      placeholder: 'Password',
+                      style: const TextStyle(color: Colors.black),
+                      obscureText: true,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 15), // Increased padding
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _login,
+                      child: const Text('Sign in',
+                          style: TextStyle(color: Colors.black)),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.blue),
                       ),
                     ),
-                  ),
-                ]
-              )
-            )
-            ],
-          ),
-      )
-    );
+                    const SizedBox(height: 20),
+                    Center(
+                      // Wrap with Center to align to the middle
+                      child: GestureDetector(
+                        onTap: () {
+                          // Add your logic for "Forget password" here
+                        },
+                        child: const Text(
+                          'Forget password?',
+                          style: TextStyle(
+                              color: Colors.black,
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    ),
+                  ]))
+        ],
+      ),
+    ));
   }
 }
-
